ffb2f06a904706c905f370d3de41a331eae1807a
Daminidhon <damini.dhondge@gmail.com>
2025-08-07 13:27:11 +0530

    updated

--- src/practice/IfElseDemo3.java
+++ src/practice/IfElseDemo3.java
@@ -1,10 +1,35 @@
 package practice;
 
+import java.util.Scanner;
+
 public class IfElseDemo3 {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		
+		boolean itsRaining=true;
+		
+		if(itsRaining==true)
+		{
+		System.out.println("i will skip class..");
+		}
+		
+		
+		System.out.println("enter two numbers--");
+		
+		Scanner sc = new Scanner (System. in);
+		
+		int a = sc.nextInt();
+		int b = sc.nextInt ();
+		
+		if(a>b)
+		{
+		System.out.println("a is greater");
+		}
+		else
+		{
+			System.out.println("b is greater");
+		}
+		sc.close();
 	}
 
 }
